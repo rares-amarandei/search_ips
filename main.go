1524764fcb3b48a91e1da557532c05cca634c19d
Rares Amarandei <rares.amarandei@leftclick.io>
2023-05-25 17:47:50 +0200

    wip

--- main.go
+++ main.go
@@ -1,12 +1,123 @@
 package main
 
 import (
+	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 
 	"leftclick.io/geolocation/geoip"
 )
+
+func scalePopulationsToQuota(cities map[string]map[string]float64, quota float64) map[string]map[string]int {
+	// Calculate total population
+	totalPopulation := 0.0
+	for _, countryCities := range cities {
+		for _, population := range countryCities {
+			totalPopulation += population
+		}
+	}
+
+	// Calculate the scaling factor
+	scalingFactor := quota / totalPopulation
+
+	// Create the new map
+	citiesInt := make(map[string]map[string]int)
+
+	for country, countryCities := range cities {
+		citiesInt[country] = make(map[string]int)
+		for city, population := range countryCities {
+			// Scale the population and round to the nearest integer
+			scaledPopulation := int(math.Round(population * scalingFactor))
+
+			citiesInt[country][city] = scaledPopulation
+		}
+	}
+
+	return citiesInt
+}
+
+func writeIntMapToJsonFile(data map[string]map[string]int, filename string) {
+	// Convert the data to JSON
+	jsonBytes, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		fmt.Printf("Error encoding to JSON: %v\n", err)
+		return
+	}
+
+	// Write the JSON data to the file
+	err = ioutil.WriteFile(filename, jsonBytes, 0644)
+	if err != nil {
+		fmt.Printf("Error writing to file: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Data written to %s\n", filename)
+}
+
+func parseInputCsv(input string) map[string]map[string]float64 {
+	// Open the input file
+	file, err := os.Open(input)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil
+	}
+	defer file.Close()
+
+	// Create a CSV reader
+	reader := csv.NewReader(file)
+
+	// Initialize the citiesByCountry map
+	citiesByCountry := make(map[string]map[string]float64)
+
+	// Read the records from the input file
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil
+	}
+
+	// Iterate over the records
+	for _, record := range records {
+		// Normalize country and city names
+		country := strings.Split(record[0], " (")[0]
+		city := strings.Split(record[1], " (")[0]
+
+		// Ignore names with a comma
+		if strings.Contains(country, ",") || strings.Contains(city, ",") {
+			continue
+		}
+
+		// Capitalize the first letter of each word
+		country = strings.Title(strings.ToLower(country))
+		city = strings.Title(strings.ToLower(city))
+
+		// Parse the population
+		population, err := strconv.ParseFloat(record[2], 64)
+		if err != nil {
+			fmt.Println("Error parsing population:", err)
+			continue
+		}
+
+		// Convert the population to millions
+		population = population / 1_000_000
+
+		// If this country hasn't been seen before, initialize it in the map
+		if _, ok := citiesByCountry[country]; !ok {
+			citiesByCountry[country] = make(map[string]float64)
+		}
+
+		// Add this city to the country's map
+		citiesByCountry[country][city] = population
+	}
+
+	return citiesByCountry
+}
 
 func intToIP(ipInt uint32) string {
 	ip := make([]byte, 4)
@@ -20,26 +131,36 @@ func intToIP(ipInt uint32) string {
 	return fmt.Sprintf("%d.%d.%d.%d", ip[3], ip[2], ip[1], ip[0])
 }
 
-type City struct {
-	Name string
-	IPs  []string
+type Country struct {
+	Cities []*City
 }
 
-func bruteForceSearch(provider geoip.Provider, citiesByCountry map[string][]string) map[string]*City {
+type City struct {
+	Name  string
+	IPs   []string
+	Quota int
+}
+
+func bruteForceSearch(provider geoip.Provider, citiesByCountry map[string]map[string]int) map[string]*Country {
 	incomplete := make(map[string]*City)
 	completed := make(map[string]*City)
 
+	countries := make(map[string]*Country)
+
 	// Initialize all cities as incomplete
 	for _, cities := range citiesByCountry {
-		for _, city := range cities {
-			incomplete[city] = &City{Name: city, IPs: []string{}}
+		for city, quota := range cities {
+			incomplete[city] = &City{Name: city, IPs: []string{}, Quota: quota}
 		}
 	}
 
-	var ip uint32 = 0
+	var ip uint32 = 1000
 	var step uint32 = 1000
 	// Start from 10000 and increase by 10000 for each iteration
 	for len(incomplete) > 0 {
+		if ip < 1000 {
+			break
+		}
 		ip += step
 		myIp := intToIP(ip)
 		info, err := provider.Lookup(myIp)
@@ -53,24 +174,52 @@ func bruteForceSearch(provider geoip.Provider, citiesByCountry map[string][]stri
 			step = 1
 			city.IPs = append(city.IPs, myIp)
 
-			// If we have collected 100 IPs for this city, move it to the completed map
-			if len(city.IPs) == 100 {
+			// If we have collected enough IPs for this city, move it to the completed map
+			if len(city.IPs) == city.Quota {
 				completed[info.City] = city
 				delete(incomplete, info.City)
 				fmt.Printf("Completed: %s, Remained: %d\n", info.City, len(incomplete))
-				if len(incomplete) < 12 {
-					for city, val := range incomplete {
-						fmt.Printf("city %s - %d\n", city, len(val.IPs))
-					}
-					fmt.Printf("current ip: %d", ip)
-				}
 			}
 		} else {
 			step = 1000
 		}
 	}
 
-	return completed
+	// If reached uint32(-1), move cities with any IPs collected to the completed map
+	if ip < 1000 {
+		for key, city := range incomplete {
+			if len(city.IPs) > 0 {
+				completed[key] = city
+				delete(incomplete, key)
+			}
+		}
+	}
+
+	file, err := os.Create("incomplete.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+	}
+	defer file.Close()
+	for key, _ := range incomplete {
+		_, err := file.WriteString(key + "\n")
+		if err != nil {
+			fmt.Println("Error writing to file:", err)
+		}
+	}
+
+	// Loop over the completed cities and group them by country
+	for country, cities := range citiesByCountry {
+		if _, ok := countries[country]; !ok {
+			countries[country] = &Country{Cities: []*City{}}
+		}
+		for cityName, _ := range cities {
+			if city, ok := completed[cityName]; ok {
+				countries[country].Cities = append(countries[country].Cities, city)
+			}
+		}
+	}
+
+	return countries
 }
 
 func main() {
@@ -81,35 +230,38 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	var citiesByCountry = map[string][]string{
-		"US": {"New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose", "Seattle", "Miami"},
-		"RU": {"Moscow", "Saint Petersburg", "Novosibirsk", "Yekaterinburg", "Kazan", "Krasnoyarsk", "Chelyabinsk", "Samara", "Omsk", "Rostov"},
-		"CN": {"Shanghai", "Beijing", "Chongqing", "Guangzhou", "Shenzhen", "Tianjin", "Chengdu", "Wuhan", "Hangzhou", "Hong Kong"},
-		"IN": {"Mumbai", "Delhi", "Bengaluru", "Hyderabad", "Ahmedabad", "Chennai", "Kolkata", "Surat", "Pune", "Jaipur"},
-		"FR": {"Paris", "Marseille", "Lyon", "Toulouse", "Nice", "Nantes", "Strasbourg", "Montpellier", "Bordeaux", "Lille"},
-		"DE": {"Berlin", "Hamburg", "Munchen", "Frankfurt am Main", "Stuttgart", "Dusseldorf", "Dortmund", "Essen", "Leipzig"},
-		"JP": {"Tokyo", "Yokohama", "Osaka", "Nagoya", "Sapporo", "Fukuoka", "Kobe", "Kyoto", "Kawasaki", "Saitama"},
-		"GB": {"London", "Birmingham", "Manchester", "Glasgow", "Newcastle", "Sheffield", "Liverpool", "Leeds", "Bristol", "Edinburgh"},
-		"KR": {"Seoul", "Busan", "Gwangmyeong", "Daegu", "Daejeon", "Gwangju", "Ulsan", "Suwon", "Seongnam", "Cheongju"},
-		"SA": {"Riyadh", "Jeddah", "Mecca", "Medina", "Dammam", "Ta'if", "Tabuk", "Abha", "Buraydah"},
-	}
-	completed := bruteForceSearch(geo, citiesByCountry)
+	inputCsv := parseInputCsv("input.csv")
+	citiesByCountry := scalePopulationsToQuota(inputCsv, 200000)
+	writeIntMapToJsonFile(citiesByCountry, "out.json")
+	countries := bruteForceSearch(geo, citiesByCountry)
 
-	file, err := os.Create("out.txt")
+	foundIps := 0
+	for _, country := range countries {
+		for _, city := range country.Cities {
+			foundIps += len(city.IPs)
+		}
+	}
+
+	jsonString, err := json.MarshalIndent(countries, "", "  ")
+	if err != nil {
+		fmt.Println("Error converting to JSON:", err)
+		return
+	}
+
+	// Write the JSON to a file
+	file, err := os.Create("countries.json")
 	if err != nil {
 		fmt.Println("Error creating file:", err)
 		return
 	}
 	defer file.Close()
 
-	for _, city := range completed {
-		output := fmt.Sprintf("City: %s, IPs: %v\n", city.Name, city.IPs)
-		_, err := file.WriteString(output)
-		if err != nil {
-			fmt.Println("Error writing to file:", err)
-			return
-		}
+	_, err = file.Write(jsonString)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
 	}
 
-	fmt.Println("Output written to file: out.txt")
+	fmt.Printf("Found ips: %d", foundIps)
+
 }
